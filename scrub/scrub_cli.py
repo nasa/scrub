c2d1a1e403da7200ab7e6d0dcc79b375e7697c06
Lyle Barner <lyle.barner@jpl.nasa.gov>
2021-09-08 11:49:29 -0700

    Updates to allow for direct scrub_cli entry.

--- scrub/scrub_cli.py
+++ scrub/scrub_cli.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from scrub import scrubme
@@ -45,4 +46,6 @@ def main():
 
 
 if __name__ == "__main__":
+    sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
     sys.exit(main())
