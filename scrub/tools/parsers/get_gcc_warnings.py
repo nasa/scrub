ce5679ff00ffcec89a7abffe987ce41bd2dadf31
Lyle Barner <lyle.barner@jpl.nasa.gov>
2022-03-03 15:08:24 -0800

    Updates to gcc parser to prevent duplicate warnings from being displayed. #43

--- scrub/tools/parsers/get_gcc_warnings.py
+++ scrub/tools/parsers/get_gcc_warnings.py
@@ -17,6 +17,14 @@ def parse_warnings(raw_input_file, parsed_output_file):
 
     # Initialize the variables
     warning_count = 1
+    warning_list = []
+    raw_warnings = []
+    warning_id = None
+    warning_file = None
+    warning_line = None
+    warning_message = []
+    parsing = False
+    description = False
 
     # Print a status message
     logging.info('')
@@ -29,22 +37,40 @@ def parse_warnings(raw_input_file, parsed_output_file):
         input_data = input_fh.readlines()
 
     # Iterate through every line of the input file
-    raw_warnings = []
     for line in input_data:
         # Find lines that contain warnings
-        if 'warning:' in line.strip():
+        if ('in function' in line.lower() or 'in file' in line.lower()) and not parsing:
+            parsing = True
+
+        if parsing and not description and 'warning:' in line.lower():
+            description = True
+
             # Split the line and store the data
             warning_file = os.path.abspath(line.split(':')[0].strip())
             warning_line = int(line.split(':')[1].strip())
-            warning_message = ['GCC Compiler Warning: ' + line.split('warning:')[-1].strip()]
+            warning_message = ['GCC Compiler Warning:', '\t' + line.rstrip()]
             warning_id = ID_PREFIX + str(warning_count).zfill(3)
 
-            # Add to the warning dictionary
-            raw_warnings.append(translate_results.create_warning(warning_id, warning_file, warning_line,
-                                                                 warning_message, ID_PREFIX, WARNING_LEVEL))
+        elif parsing and description and line.lower().startswith(' '):
+            warning_message.append('\t' + line.rstrip())
 
-            # Increment the warning count
-            warning_count = warning_count + 1
+        elif parsing and description and not line.lower().startswith(' '):
+            parsing = False
+            description = False
+
+            # Check to see if the warning is in the list
+            warning = [warning_file, warning_line, warning_message]
+            if warning not in warning_list:
+                # Add the warning to the list
+                warning_list.append(warning)
+                raw_warnings.append(translate_results.create_warning(warning_id, warning_file, warning_line,
+                                                                     warning_message, ID_PREFIX, WARNING_LEVEL))
+
+                # Increment the warning count
+                warning_count = warning_count + 1
+
+            else:
+                logging.info('\t>> Duplicate warning omitted.')
 
     # Create the SCRUB output file
     translate_results.create_scrub_output_file(raw_warnings, parsed_output_file)
