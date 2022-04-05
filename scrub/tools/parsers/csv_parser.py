fe49dd3b5d9240346ad73f3b66bc201bb7a95abc
Lyle Barner <lyle.barner@jpl.nasa.gov>
2022-04-05 11:15:08 -0700

    Updates to parser

--- scrub/tools/parsers/csv_parser.py
+++ scrub/tools/parsers/csv_parser.py
@@ -9,7 +9,7 @@ from scrub.tools.parsers import translate_results
 def parse_warnings(input_dir):
     # Initialize variables
     source_dir = os.path.dirname(input_dir)
-    output_dir = os.path.normpath(input_dir + '/vatt_output')
+    output_dir = os.path.normpath(input_dir + '/csv_output')
 
     # Make the output directory if it doesn't already exist
     if os.path.exists(output_dir):
@@ -19,19 +19,19 @@ def parse_warnings(input_dir):
     # Get a list of all the scrub files in the directory
     input_files = glob.glob(input_dir + '/*.scrub')
 
-    # Iterate through every file
-    for input_file in input_files:
-        # Create the output file path
-        output_file = (output_dir + '/' + datetime.datetime.utcnow().strftime("%m-%d-%Y") + '_' +
-                       os.path.basename(input_file).replace('.scrub', '') + '.csv')
+    # Create the output file path
+    output_file = (output_dir + '/' + datetime.datetime.utcnow().strftime("%m-%d-%Y") + '.csv')
 
-        # Write the results out to the csv file
-        with open(output_file, 'w') as output_fh:
+    # Write the results out to the csv file
+    with open(output_file, 'w') as output_fh:
+        # Write out the header data
+        output_fh.write('ID,Query,Description,Priority,File,Line\n')
+
+        # Iterate through every file
+        for input_file in input_files:
+
             # Parse the SCRUB files
             scrub_results = translate_results.parse_scrub(input_file, source_dir)
-
-            # Write out the header data
-            output_fh.write('ID,Query,Description,Priority,File,Line\n')
 
             # Iterate through every result
             for result in scrub_results:
