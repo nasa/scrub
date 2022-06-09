9215217e621b9c655a86f7f736bd67c6bd5c687c
Lyle Barner <lyle.barner@jpl.nasa.gov>
2022-06-09 14:17:34 -0700

    Updates to retrieve/parse hotspot findings as well #58

--- scrub/tools/parsers/get_sonarqube_warnings.py
+++ scrub/tools/parsers/get_sonarqube_warnings.py
@@ -20,35 +20,49 @@ def parse_warnings(results_dir, parsed_output_file, source_root):
     warning_count = 1
     raw_warnings = []
 
-    # Find all the raw results files in the directory
-    results_files = glob.glob(results_dir + '/sonarqube_warnings_*.json')
+    # Find all the raw issues results files in the directory
+    findings_results_files = glob.glob(results_dir + '/*.json')
 
-    # Iterate through every results file
-    for raw_input_file in results_files:
+    # Iterate through every issues results file
+    for raw_findings_file in findings_results_files:
         # Read in the input data
-        with open(raw_input_file, 'r') as input_fh:
+        with open(raw_findings_file, 'r') as input_fh:
             input_data = json.loads(input_fh.read())
 
         # Iterate through every finding in the input file
-        for issue in input_data['issues']:
+        if 'issues' in input_data.keys():
+            findings = input_data['issues']
+        else:
+            findings = input_data['hotspots']
+        for finding in findings:
+            # Check to see if the warning should be suppressed
+            if 'resolution' in finding.keys():
+                suppression = True
+            else:
+                suppression = False
+
             # Parse the finding
-            warning_file = os.path.normpath(source_root + '/' + issue['component'].split(':')[-1])
-            warning_message = issue['message'].splitlines()
+            warning_file = os.path.normpath(source_root + '/' + finding['component'].split(':')[-1])
+            warning_message = finding['message'].splitlines()
             warning_id = ID_PREFIX + str(warning_count).zfill(3)
-            warning_query = issue['rule'].replace(':', '-')
+
+            if 'rule' in finding.keys():
+                warning_query = finding['rule'].replace(':', '-')
+            else:
+                warning_query = ''
 
             # Get the line number
-            if 'line' in issue.keys():
-                warning_line = int(issue['line'])
-            elif 'textRange' in issue.keys():
-                warning_line = int(issue['textRange']['startLine'])
+            if 'line' in finding.keys():
+                warning_line = int(finding['line'])
+            elif 'textRange' in finding.keys():
+                warning_line = int(finding['textRange']['startLine'])
             else:
                 warning_line = 0
 
             # Add to the warning dictionary
             raw_warnings.append(translate_results.create_warning(warning_id, warning_file, warning_line,
                                                                  warning_message, ID_PREFIX, WARNING_LEVEL,
-                                                                 warning_query))
+                                                                 warning_query, suppression))
 
             # Increment the warning count
             warning_count = warning_count + 1
