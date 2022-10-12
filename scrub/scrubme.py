98285d5e926aa31cddfb8c1b680b1b0922dac852
Lyle Barner <lyle.barner@jpl.nasa.gov>
2022-10-12 15:00:12 -0700

    Updates for test cases

--- scrub/scrubme.py
+++ scrub/scrubme.py
@@ -81,7 +81,7 @@ def main(conf_file=pathlib.Path('./scrub.cfg').resolve(), clean=False, console_l
 
     # Make sure the working directory exists
     if not scrub_conf_data.get('scrub_working_dir').exists():
-        print('ERROR: Working directory ' + scrub_conf_data.get('scrub_working_dir') + ' does not exist.')
+        print('ERROR: Working directory ' + str(scrub_conf_data.get('scrub_working_dir')) + ' does not exist.')
         sys.exit(10)
 
     # Make a copy of the scrub.cfg file and add it to the log
