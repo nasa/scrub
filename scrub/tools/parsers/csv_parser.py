709e834a4900385c839223c6aab02519da3b95c5
Lyle Barner <lyle.barner@jpl.nasa.gov>
2022-12-01 13:15:37 -0800

    Updates to csv parser

--- scrub/tools/parsers/csv_parser.py
+++ scrub/tools/parsers/csv_parser.py
@@ -5,7 +5,7 @@ import pathlib
 from scrub.tools.parsers import translate_results
 
 
-def parse_warnings(input_dir):
+def parse_warnings(input_dir, output_format='legacy'):
     # Initialize variables
     source_dir = pathlib.Path(input_dir)
     output_dir = source_dir.joinpath('csv_output')
@@ -21,27 +21,43 @@ def parse_warnings(input_dir):
     # Create the output file path
     output_file = output_dir.joinpath(datetime.datetime.utcnow().strftime("%m-%d-%Y") + '.csv')
 
-    print(output_file)
-
     # Write the results out to the csv file
     with open(output_file, 'w') as output_fh:
-        # Write out the header data
-        output_fh.write('ID,Query,Description,Priority,File,Line\n')
+        if output_format == 'generic':
+            # Write out the header data
+            output_fh.write('ID,Query,Description,Priority,File,Line\n')
 
-        # Iterate through every file
-        for input_file in input_files:
+            # Iterate through every file
+            for input_file in input_files:
 
-            # Parse the SCRUB files
-            scrub_results = translate_results.parse_scrub(input_file, source_dir)
+                # Parse the SCRUB files
+                scrub_results = translate_results.parse_scrub(input_file, source_dir)
 
-            # Iterate through every result
-            for result in scrub_results:
-                output_fh.write('{},{},{},{},{},{}\n' .format(result.get('id'), result.get('query'),
-                                                              ' '.join(result.get('description')).replace(',', ''),
-                                                              result.get('priority'),
-                                                              result.get('file').relative_to(source_dir),
-                                                              result.get('line')))
+                # Iterate through every result
+                for result in scrub_results:
+                    output_fh.write('{},{},{},{},{},{}\n'.format(result.get('id'), result.get('query'),
+                                                                 ' '.join(result.get('description')).replace(',', ''),
+                                                                 result.get('priority'),
+                                                                 result.get('file').relative_to(source_dir),
+                                                                 result.get('line')))
+
+        else:
+            # Iterate through every file
+            for input_file in input_files:
+
+                # Parse the SCRUB files
+                scrub_results = translate_results.parse_scrub(input_file, source_dir)
+
+                # Iterate through every result
+                for result in scrub_results:
+                    description_text = ' '.join(result.get('description')).replace(',', '').replace('\t', '').strip()
+                    output_fh.write('{},{},{},{},{},{},{},{},{},{}\n'.format(result.get('query'), description_text,
+                                                                             result.get('priority'), description_text,
+                                                                             result.get('file').relative_to(source_dir),
+                                                                             result.get('file').relative_to(source_dir),
+                                                                             result.get('line'), result.get('line'), 0,
+                                                                             0))
 
 
 if __name__ == '__main__':
-    parse_warnings(pathlib.Path(sys.argv[1]).resolve())
+    parse_warnings(pathlib.Path(sys.argv[1]).resolve(), sys.argv[2])
