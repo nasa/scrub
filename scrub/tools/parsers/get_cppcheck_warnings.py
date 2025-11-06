cdacffdf9b60ee53ee616935b86d10b6ff73a20e
Lyle Barner <lyle.barner@jpl.nasa.gov>
2025-11-06 09:47:25 -0800

    Updates to use custom output template instead of XML

--- scrub/tools/parsers/get_cppcheck_warnings.py
+++ scrub/tools/parsers/get_cppcheck_warnings.py
@@ -1,6 +1,5 @@
 import pathlib
 import logging
-import xml.etree.ElementTree
 from scrub.tools.parsers import translate_results
 
 ID_PREFIX = 'cppcheck'
@@ -22,14 +21,15 @@ def parse_warnings(analysis_dir, tool_config_data, raw_input_file=None, parsed_o
 
     # Set the input file
     if raw_input_file is None:
-        raw_input_file = analysis_dir.joinpath('cppcheck_output.xml')
+        raw_input_file = analysis_dir.joinpath('cppcheck_output.txt')
 
     # Set the output file
     if parsed_output_file is None:
         parsed_output_file = tool_config_data.get('raw_results_dir').joinpath('cppcheck_compiler_raw.scrub')
 
     # Read in the input data
-    raw_input_data = xml.etree.ElementTree.parse(raw_input_file).getroot()
+    with open(raw_input_file, 'r') as input_fh:
+        raw_input_data = input_fh.readlines()
 
     # Print a status message
     logging.info('\t>> Executing command: get_cppcheck_warnings.parse_warnings(%s, %s)', analysis_dir,
@@ -38,16 +38,19 @@ def parse_warnings(analysis_dir, tool_config_data, raw_input_file=None, parsed_o
 
     # Iterate through every finding in the input file
     raw_warnings = []
-    for finding in raw_input_data.find('errors').findall('error'):
-        # Make sure we haven't hit the metadata
-        if finding.get('id') != 'checkersReport':
-            # Parse the finding
-            warning_file = pathlib.Path(finding.find('location').get('file')).resolve()
-            warning_line = int(finding.find('location').get('line'))
-            warning_message = finding.get('verbose')
+    for line in raw_input_data:
+        # Parse the finding
+        finding = line.strip().split('::')
+
+        # Add everything that has a file location
+        if finding[0] != 'nofile':
+            warning_file = pathlib.Path(finding[0]).resolve()
+            warning_line = int(finding[1])
+            warning_message = [finding[-1]]
             warning_id = ID_PREFIX + str(warning_count).zfill(3)
-            warning_type = finding.get('id')
-            warning_severity = finding.get('severity')
+            warning_severity = finding[2]
+            warning_type = finding[3]
+            cwe = int(finding[4])
 
             # Set the warning level based on the severity
             if warning_severity == 'error':
@@ -57,9 +60,14 @@ def parse_warnings(analysis_dir, tool_config_data, raw_input_file=None, parsed_o
             else:
                 warning_level = 'Low'
 
+            # Add CWE data if applicable
+            if cwe != 0:
+                warning_message.append('Related CWE: {}'.format(cwe))
+
             # Add to the warning dictionary
-            raw_warnings.append(translate_results.create_warning(warning_id, warning_file, warning_line, warning_message,
-                                                                 ID_PREFIX, warning_level, warning_type))
+            raw_warnings.append(translate_results.create_warning(warning_id, warning_file, warning_line,
+                                                                 warning_message, ID_PREFIX, warning_level,
+                                                                 warning_type))
 
             # Increment the warning count
             warning_count = warning_count + 1
