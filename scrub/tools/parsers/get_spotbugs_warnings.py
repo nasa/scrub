f9ff7ed171dd4e227c9e732dad1d15d0aeae58d7
Lyle Barner <lyle.barner@jpl.nasa.gov>
2025-11-11 16:15:43 -0800

    Updates to support SpotBugs analysis.

---
+++ scrub/tools/parsers/get_spotbugs_warnings.py
@@ -0,0 +1,37 @@
+import pathlib
+import logging
+from scrub.tools.parsers import translate_results
+
+
+def parse_warnings(analysis_dir, tool_config_data, raw_input_file=None, parsed_output_file=None):
+    """This function handles parsing of raw SpotBugs data.
+
+    Inputs:
+        - analysis_dir: Absolute path to the raw SpotBugs output file directory [string]
+        - tool_config_data: Dictionary of scrub configuration data [dict]
+        - raw_input_file: Absolute path to the raw input file [string] [optional]
+        - parsed_output_file: Absolute path to the raw output file [string] [optional]
+    """
+
+    # Initialize variables
+    source_dir = tool_config_data.get('source_dir')
+
+    # Set the input file
+    if raw_input_file is None:
+        raw_input_file = analysis_dir.joinpath('spotbugs.sarif')
+
+    # Set the output file
+    if parsed_output_file is None:
+        parsed_output_file = tool_config_data.get('raw_results_dir').joinpath('spotbugs_raw.scrub')
+
+    # Print a status message
+    logging.info('\t>> Executing command: get_spotbugs_warnings.parse_warnings(%s, %s)', analysis_dir,
+                 parsed_output_file)
+    logging.info('\t>> From directory: %s', str(pathlib.Path().absolute()))
+
+    # Parse the SARIF file
+    raw_warnings = translate_results.parse_sarif(raw_input_file, source_dir)
+
+    # Create the SCRUB output file
+    translate_results.create_scrub_output_file(raw_warnings, parsed_output_file)
+
